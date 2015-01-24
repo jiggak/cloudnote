ffdbd9156d5a87113e6c9cf8e378736e4b8f9bfb
Josh Kropf <josh@slashdev.ca>
2015-01-23 20:52:48 -0500

    fix webdav xml traversal in firefox

--- src/js/factory/Note.js
+++ src/js/factory/Note.js
@@ -1,9 +1,9 @@
 notesApp.factory('Note', function ($http) {
   function Note(response) {
-    this.filePath = $(response).find('href').text();
+    this.filePath = $(response).find('D\\:href, href').text();
     this.fileName = unescape(this.filePath.split('/').pop());
     this.name = unescape(this.fileName.replace('.md', ''));
-    this.etag = $(response).find('getetag').text();
+    this.etag = $(response).find('lp1\\:getetag, getetag').text();
   }
 
   Note.prototype.get = function() {
--- src/js/service/notes.js
+++ src/js/service/notes.js
@@ -6,7 +6,7 @@ notesApp.service('notes', function ($q, $rootScope, $http, root,  Note) {
       jQuery.Dav(root).readFolder({
         error: console.log,
         success: function (data) {
-          var notes = _.chain($(data).find('response'))
+          var notes = _.chain($(data).find('D\\:response, response'))
             // first child is properties response of the directory being listed
             // root etag can be used to decide if the directory listing is stale
             // $(value).find('getetag').text();
